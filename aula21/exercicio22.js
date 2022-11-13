5c9376226f496f34377fe024924e40f7878b3473
Reynan Paiva <reynanwq@gmail.com>
2022-11-13 15:59:07 -0300

    exercicio22

---
+++ aula21/exercicio22.js
@@ -0,0 +1,8 @@
+let num1 = prompt('Digite um numero: ');
+let num2 = prompt('Digite um numero: ');
+
+//converter
+num1 = Number(num1);
+num2 = Number(num2);
+const resultado = num1 + num2;
+alert(`o resultado da soma desses dois números foi : ${resultado}`);
