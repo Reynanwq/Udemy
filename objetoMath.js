bb09f86a49e9edc1142b3954b070df6d8d8f6382
Reynan Paiva <reynanwq@gmail.com>
2022-11-13 19:14:07 -0300

    exercicio25 + Math + trabalhando[strings]

---
+++ objetoMath.js
@@ -0,0 +1,8 @@
+let num = 9.21;
+
+let num1 = Math.floor(num); //arredonda para cima
+console.log(num1);
+let num2 = Math.ceil(num); //arredonda para baixo
+console.log(num2);
+let num3 = Math.round(num); //arredonda para o mais próximo
+console.log(num3);
---
+++ strings.js
@@ -0,0 +1,6 @@
+let umaString = "Reynan Reynan Rabanete Rosquinha";
+
+console.log(umaString.replace(/R/g, '$'));
+console.log(umaString.length); //tamanho
+console.log(umaString.toUpperCase()); //maiuscculo
+console.log(umaString.toLowerCase()); //minusculo
