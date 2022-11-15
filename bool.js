1d56371eba79e9ab51378b0f484b97610f734142
Reynan Paiva <reynanwq@gmail.com>
2022-11-15 08:08:20 -0300

    models + algumas operacoes em booleano

---
+++ bool.js
@@ -0,0 +1,8 @@
+const User = 'Reynanwq';
+const password = 12345678;
+
+if (User == 'Reynanwq' && password == 12345678) {
+    console.log('logou!');
+} else {
+    console.log("User ou Password incorretos.");
+}
