ec071e0b12aa9e9ea45f8023952b828f07ab80db
Reynan Paiva <reynanwq@gmail.com>
2022-12-19 20:04:57 -0300

    avdOBJECTO

---
+++ Dezembro/avdObjeto.js
@@ -0,0 +1,10 @@
+const pessoa = {
+    nome: 'Reynan',
+    sobrenome: 'paiva',
+    edndereco: {
+        rua: 'santo amaro',
+        numero: 11
+    }
+};
+const { nome, sobrenome, edndereco } = pessoa;
+console.log(edndereco);
