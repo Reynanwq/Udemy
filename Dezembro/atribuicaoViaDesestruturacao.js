7a5df96cbb0001e013c6c128cb518c9a06676fca
Reynan Paiva <reynanwq@gmail.com>
2022-12-19 19:16:53 -0300

    att via desestruturacao

---
+++ Dezembro/atribuicaoViaDesestruturacao.js
@@ -0,0 +1,3 @@
+const number = [10, , 20, , 25, 30]
+const [dez, , vinte, , vinteCinco, trinta] = number
+console.log(dez, vinte, vinteCinco, trinta)
---
+++ Dezembro/atribuicaoViaDesestruturacao2.js
@@ -0,0 +1,8 @@
+const number = [
+    [10, 15, 20],
+    [25, 30, 35],
+    [40, 45, 50]
+];
+const [lista1, lista2, lista3] = number;
+console.log(lista2);
+console.log(lista2[1]);
