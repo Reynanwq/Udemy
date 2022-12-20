dbad1ff6fffab48866dc3be231c7eb483340f326
Reynan Paiva <reynanwq@gmail.com>
2022-12-19 22:06:55 -0300

    while + break + continues

--- Dezembro/break&continues.js
+++ Dezembro/break&continues.js
@@ -10,16 +10,20 @@ for (let number of numbers) {
         break;
     }
 }
+
 let i = 0;
 while (i < numbers.length) {
     let number = numbers[i];
     if (number === 2) {
         console.log('pulei o number two');
+        i++;
         continue;
     }
     console.log(number);
     if (number === 6) {
         console.log('elemento 6 encontrado, codigo finalizando....');
+        i++;
         break;
     }
+    i++;
 }
