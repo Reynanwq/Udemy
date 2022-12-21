284119ffeb4326ad8f99fc486031fbd6ee79d3a3
Reynan Paiva <reynanwq@gmail.com>
2022-12-21 00:51:35 -0300

    exibindo no console.log as info do json

--- No-Udemy/json/json.js
+++ No-Udemy/json/json.js
@@ -1,1 +1,20 @@
 /*O QUE É JSON*/
+
+//json.parse() -> converte texto json em objetos
+//json.stringfy() -> Converter objetos em texto padrão json
+
+const car = {
+    marca: "Ferrari",
+    modelo: "Ferrari 488",
+    ano: 2020,
+    Peso: "1475 Kg",
+    Potência: 669,
+    Cilindrada: "3902 cm³"
+}
+let converteTexto = JSON.stringify(car);
+document.getElementById('area').innerHTML = converteTexto;
+
+//convertere novamente para objeto
+let converteObjeto = JSON.parse(converteTexto);
+console.log(car.marca);
+console.log(car.Cilindrada);
