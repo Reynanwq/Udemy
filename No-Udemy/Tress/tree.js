d20303c5fca32c0f121458420c75470178bd42a8
Reynan Paiva <reynanwq@gmail.com>
2022-12-22 00:17:51 -0300

    operacao insercao

--- No-Udemy/Tress/tree.js
+++ No-Udemy/Tress/tree.js
@@ -10,3 +10,27 @@ class NoAVL {
         this.altura = 0;
     }
 }
+
+class ArvoreAVL {
+    constructor() {
+        this.raiz = null;
+    }
+}
+
+insert(chave, dado) {
+    this.raiz = this.insert_aux(chave, dado);
+}
+
+
+insert_aux(chave, dado, no = this.raiz) {
+    //verifica se o NÓ é nulo
+    if (no === null) {
+        no = new NoAVL(chave, dado);
+    } else if (chave < no.chave) {
+        no.esquerda = this.insert_aux(chave, dado, no.esquerda);
+    } else if (chave > no.chave) {
+        no.direita = this.insert_aux(chave, dado, no.direita);
+    } else {
+        throw Error("Chave já definida ou não numerica");
+    }
+}
