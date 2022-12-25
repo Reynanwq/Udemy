d8ccb3613496774a1a630252ceaba489f0a2dba7
Reynan Paiva <reynanwq@gmail.com>
2022-12-25 11:52:59 -0300

    funcionalidades aplicadas

--- Dezembro/Timer/script.js
+++ Dezembro/Timer/script.js
@@ -4,15 +4,38 @@ const timer = document.querySelector('.timer');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
+let segundos = 0;
+let time;
+//FUNCAO QUE PEGA MEUS SEGUNDO E PASSA PARA O FORMATO HORÁRIO
+
+function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+    });
+}
+
+function iniciaRelogio() {
+    time = setInterval(function() {
+        segundos++;
+        timer.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000);
+
+}
+
 
 iniciar.addEventListener('click', function(event) {
-    alert('Cliquei no inciar');
+    clearInterval(time);
+    iniciaRelogio();
 });
 
-pausar.addEventListener('click', function() {
-    alert('Cliquei no pausar');
+pausar.addEventListener('click', function(event) {
+    clearInterval(time);
 });
 
-zerar.addEventListener('click', function() {
-    alert('Cliquei no zerar');
+zerar.addEventListener('click', function(event) {
+    clearInterval(time);
+    timer.innerHTML = '00:00:00';
+    segundos = 0;
 });
