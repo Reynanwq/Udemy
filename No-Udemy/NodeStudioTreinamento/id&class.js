66ec95414c223df56f501d9044529c06b630ed32
Reynan Paiva <reynanwq@gmail.com>
2022-12-27 14:40:44 -0300

    using className

---
+++ No-Udemy/NodeStudioTreinamento/id&class.js
@@ -0,0 +1,6 @@
+const fruta = document.getElementById('fruta');
+const carro = document.getElementsByClassName('carro')[0];
+const moto = document.getElementsByClassName('moto')[0];
+console.log(fruta);
+console.log(carro);
+console.log(moto);
