7f47285bb8c73b19d3de0b2bbe6ba34d181667c4
Reynan Paiva <reynanwq@gmail.com>
2022-12-26 22:39:33 -0300

    [add]script

--- Dezembro/Lista de Tarefas/script.js
+++ Dezembro/Lista de Tarefas/script.js
@@ -3,8 +3,12 @@ const inputTarefa = document.querySelector('.input-nova-tarefa');
 const btnTarefa = document.querySelector('.btn-add-tarefa');
 const tarefa = document.querySelector('.tarefa');
 
+function criarTarera(textoInput) {
+    //exibir o que foi digitado no input text 
+    console.log(textoInput);
+}
+
 btnTarefa.addEventListener('click', function() {
     if (!inputTarefa.value) return;
-    //exibir o que foi digitado no input text 
-    console.log(inputTarefa.value);
+    criarTarera(inputTarefa.value);
 });
