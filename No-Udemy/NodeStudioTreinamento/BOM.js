2e5c31bcd13e6e3a6b6220a36a199ca903fb7731
Reynan Paiva <reynanwq@gmail.com>
2022-12-26 23:16:46 -0300

    O que é BOM

---
+++ No-Udemy/NodeStudioTreinamento/BOM.js
@@ -0,0 +1,6 @@
+/*Browser Object Model (BOM)
+
+Window -> objeto que tá no top da árvore*/
+
+//me retorna a largura da janela, como o WINDOW é o principal, então podemos ignorar ele
+console.log(window.screen.width);
