4217bdddc93c29dd27824b62015ceacac6eebd71
Reynan Paiva <reynanwq@gmail.com>
2022-12-26 23:20:46 -0300

    navigator

--- No-Udemy/NodeStudioTreinamento/BOM.js
+++ No-Udemy/NodeStudioTreinamento/BOM.js
@@ -3,4 +3,7 @@
 Window -> objeto que tá no top da árvore*/
 
 //me retorna a largura da janela, como o WINDOW é o principal, então podemos ignorar ele
-console.log(window.screen.width);
+console.log('A largura da minha página é de: ' + window.screen.width);
+
+//retorna a versão
+console.log('A versão é a: ' + navigator.appVersion);
