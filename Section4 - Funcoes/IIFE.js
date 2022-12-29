32eb4830a245fe5c3b604360cc6083cb76628035
Reynan Paiva <reynanwq@gmail.com>
2022-12-29 14:21:10 -0300

    funcao invocada assim que é  criada

---
+++ Section4 - Funcoes/IIFE.js
@@ -0,0 +1,8 @@
+/* IIFE : Immediately invoked function expression
+Função que é invocada imediatamente assim que ela é criada
+*/
+
+(function() {
+    const eu = 'Reynan';
+    console.log(eu);
+})();
