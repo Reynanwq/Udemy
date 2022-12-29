cd772e2136e272b556bb7fa151ca1cdd62490273
Reynan Paiva <reynanwq@gmail.com>
2022-12-28 23:48:14 -0300

    sintaxe

---
+++ Section4 - Funcoes/sintaxebasica.js
@@ -0,0 +1,15 @@
+const player = 'Deft';
+
+function nomeDoPlayer() {
+    const player = 'Viper';
+    console.log(player);
+}
+
+function chamaNomeDoPlayer() {
+    nomeDoPlayer()
+}
+
+chamaNomeDoPlayer();
+/* A funcao chama o nome do viper primeiro, pois
+Primeiro a funcao busca o que está dentro dela e depois o que está fora
+*/
