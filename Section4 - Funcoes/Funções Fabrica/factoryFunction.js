d324341408df5d69ba365481fc773c232b0702a7
Reynan Paiva <reynanwq@gmail.com>
2023-01-02 20:11:02 -0300

    get e set - function

--- Section4 - Funcoes/Funções Fabrica/factoryFunction.js
+++ Section4 - Funcoes/Funções Fabrica/factoryFunction.js
@@ -3,12 +3,24 @@ function criaPessoa(nome, sobreNome, altura, peso) {
     return {
         nome,
         sobreNome,
+        /*------------ ATENÇÃO NO USO DOS METODOS GETTER E SETTER QUE FORAM USADOS NAS FUNÇÕES -----------*/
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobreNome}`;
+        },
+        set nomeCompleto(valor) {
+            //objetivo: Pegar um nome completo e separarr entre Nome e SobreNome
+            valor = valor.split(' '); //a cada espaço
+            this.nome = valor.shift(); //shift é um metodo que pega o primeiro valor e ainda remove ele do array
+            this.sobreNome = valor.join(' '); //Pega tudo que está separado por virgula, tirando do array 
+            //console.log(valor);
+        },
+
         fala: function(assunto) {
             return `${this.nome} está ${assunto}`
         },
         altura: altura,
         peso: peso,
-        imc() {
+        get imc() { //get imc permite que eu acesse  imc  como se fosse um atributo (só vou utilizar isso a partir de agora)
             const indice = this.peso / (this.altura ** 2);
             return indice.toFixed(2);
         }
@@ -17,8 +29,15 @@ function criaPessoa(nome, sobreNome, altura, peso) {
 
 const pesssoa = criaPessoa('Reynan', 'Paiva', 1.80, 60);
 const pesssoa2 = criaPessoa('Renata', 'Santana', 1.65, 52);
+
+
+
 //console.log(pessoa.nome);
+pesssoa.nomeCompleto = 'Reynan Da Silva Dias Paiva';
+console.log(pesssoa.nomeCompleto);
 console.log(pesssoa.fala('Falando sobre Java'));
-console.log('O seu IMC é: ' + pesssoa.imc())
+console.log('O seu IMC é: ' + pesssoa.imc)
+pesssoa2.nomeCompleto = 'Renata Santana';
+console.log(pesssoa2.nomeCompleto);
 console.log(pesssoa2.fala('Falando sobre Java'));
-console.log('O seu IMC é: ' + pesssoa2.imc())
+console.log('O seu IMC é: ' + pesssoa2.imc)
