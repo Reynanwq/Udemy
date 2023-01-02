f6bf6baff96420e5ab1f1cb29f884deb4bfb061c
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2023-01-02 17:50:29 -0300

    [add] text

---
+++ No-Udemy/NodeJS/What's Node.js
@@ -0,0 +1,21 @@
+/*
+Ambiente que permite navegação de código javascript fora do navegador
+V8 Engine: transforma o código javascript em código de máquina
+É UTILIZADO PARA CONSTTRUIR API
+
+APESAR DE TER APENASS 1 NÚCLEO, O NODE É CAPAZ DE EXECUTAR TAREFAS SIMULTANEAS.
+
+Harmonia coom front-end (pois ambos utilizam javascript)
+Rapido, simples e escalavel
+
+Podemos criar modulos e importá-los
+(Path, fs, http etc..)
+
+CommonJS: utilizado para importar um modulo
+
+const path = require('path');
+const meuArquivo = require('./meu-arquivo.js');
+
+NPM - NODE PACKGE MANAGER: Permite que instale modulos de terceiros
+o arquivo "package.json" lista todos os modulos que foram instalados pelo NPM
+*/
