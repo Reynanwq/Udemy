96d28b29a36b7b6de7e32eda56fe8cc2f43a1f4e
Reynan Paiva <reynanwq@gmail.com>
2023-01-03 23:35:49 -0300

    botao clear adicionado

--- Section4 - Funcoes/Calculator/calc.js
+++ Section4 - Funcoes/Calculator/calc.js
@@ -1,17 +1,29 @@
 function criaCalculadora() {
     /*Metodos abaixo e atributo acima*/
     //NO query selector sempre deve selecionar a clasee com o ".nome"
-    display: document.querySelector('.display');
-
     return {
+        display: document.querySelector('.display'),
+        clearDisplay() {
+            this.display.value = '';
+        },
         inicia() {
-            alert("Calculadora Inicializada");
+            this.cliqueBotoes();
+            this.clearDisplay();
         },
         cliqueBotoes() {
-            document.addEventListener('click', function(e) {
+            document.addEventListener('click', e => { //precisou da arrow function para funcionar
                 const el = e.target; //ou seja, serve para indicar qual documento nós estamos clicando, pois o "document" pega tudo na paágina.
+                if (el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
+                }
+                if (el.classList.contains('btn-clear')) {
+                    this.clearDisplay();
+                }
             });
-        }
+        },
+        btnParaDisplay(value) {
+            this.display.value += value;
+        },
     };
 }
 
