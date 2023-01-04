1666a0f7b2eb00d87af37970c9b619a7b707cec5
Reynan Paiva <reynanwq@gmail.com>
2023-01-04 00:14:45 -0300

    calculado finalizada + add realiza conta + bota enter

--- Section4 - Funcoes/Calculator/calc.js
+++ Section4 - Funcoes/Calculator/calc.js
@@ -3,15 +3,41 @@ function criaCalculadora() {
     //NO query selector sempre deve selecionar a clasee com o ".nome"
     return {
         display: document.querySelector('.display'),
+        inicia() {
+            this.cliqueBotoes();
+            this.clearDisplay();
+            this.pressEnter();
+        },
+
         clearDisplay() {
             this.display.value = '';
         },
         apagaUm() {
             this.display.value = this.display.value.slice(0, -1); //slice: recebe a string e tira 1 elemento dessa string.
         },
-        inicia() {
-            this.cliqueBotoes();
-            this.clearDisplay();
+
+        pressEnter() {
+            this.display.addEventListener('keyup', e => { //keyup: tecla enter
+                if (e.keyCode == 13) {
+                    this.realizaConta();
+                }
+            });
+        },
+
+        realizaConta() {
+            //UTILIZAR FUNÇÃO EVAL (MUITO PERIGOSA, POIS ELA PEGA QUALQUER COISA E JÁ TRANSFORMA PARA JAVASCRIPT)
+            let conta = this.display.value;
+            try {
+                conta = eval(conta);
+                if (!conta) {
+                    alert('Conta Invalida');
+                    return;
+                }
+                this.display.value = String(conta);
+            } catch (e) {
+                alert('Conta Invalida');
+                return;
+            }
         },
         cliqueBotoes() {
             document.addEventListener('click', e => { //precisou da arrow function para funcionar
@@ -24,6 +50,9 @@ function criaCalculadora() {
                 }
                 if (el.classList.contains('btn-del')) {
                     this.apagaUm();
+                }
+                if (el.classList.contains('btn-eq')) {
+                    this.realizaConta();
                 }
             });
         },
