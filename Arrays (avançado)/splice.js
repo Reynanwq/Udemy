a14d051cd28c145045671d21718bf2f4571722bc
Reynan Paiva <reynanwq@gmail.com>
2023-01-06 21:56:39 -0300

    splice

---
+++ Arrays (avançado)/splice.js
@@ -0,0 +1,11 @@
+/*
+nomes.splice(quando começar, quando finalizade).
+função começa em...
+função termina...
+
+NUMBER.MAX_VALUE: referencia ao MAX só que no javascrpt
+*/
+
+let nomes = ['reynan', 'joao', 'pedro'];
+console.log(nomes.splice(2, 1)); //remove joao
+console.log(nomes);
