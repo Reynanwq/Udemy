3c76d2f291311e1ec2c77d42a3f6ed612019e53d
Reynan Paiva <reynanwq@gmail.com>
2023-01-08 22:23:01 -0300

    add all server's in the index

--- No-Udemy/NodeJS/Training/index.js
+++ No-Udemy/NodeJS/Training/index.js
@@ -1,1 +1,10 @@
+require('./modules/t1');
+require('./modules/dk');
+require('./modules/drx');
+require('./modules/geng');
+require('./modules/kdf');
+require('./modules/kt');
+require('./modules/lsb');
+require('./modules/ns');
+require('./modules/brion');
 require('./modules/hle');
