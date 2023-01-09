1c69c0661dccb656ecfd3231e325348c87498f83
Reynan Paiva <reynanwq@gmail.com>
2023-01-09 15:38:29 -0300

    comentando no primo.js

--- No-Udemy/Dev Aprender/primo.js
+++ No-Udemy/Dev Aprender/primo.js
@@ -1,16 +1,15 @@
-exibirPrimos(15);
-
 function exibirPrimos(value) {
-    for (let number = 0; number < value; number++) {
+    for (let number = 0; number < value; number++) { //4
         if (ehPrimo(number)) console.log(number);
     }
 }
 
 function ehPrimo(value) {
-    for (let verifica = 2; verifica < value; verifica++) {
+    for (let verifica = 2; verifica < value; verifica++) { // 5 < 4 ?
         if (value % verifica == 0) {
             return false;
         }
     }
     return true;
 }
+exibirPrimos(4);
