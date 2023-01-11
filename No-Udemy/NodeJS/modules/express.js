6e42118cfe1f26f88fad2c606276831dd7b917df
Reynan Paiva <reynanwq@gmail.com>
2023-01-11 20:42:44 -0300

    realizando operações no banco de dados

--- No-Udemy/NodeJS/modules/express.js
+++ No-Udemy/NodeJS/modules/express.js
@@ -2,15 +2,17 @@ const express = require('express');
 const UserModel = require("../src/model/user.model");
 
 const app = express();
+app.use(express.json()); //reconhecer que sempre usamos json nas nossas requisições 
 
-app.get('/users', (req, res) => {
+app.get('/users', async(req, res) => {
     /*res.contentType("application/html"); //para passar como html
     res.status(200).send("<h1>Hello Express</h1>");*/
-    const users = [{
-        name: "reyynan",
-        email: 'reynanwq@gmail.com',
-    }, ];
-    res.status(200).json(users);
+    try {
+        const users = await UserModel.find({}); //deixar vázio para pegar tudo
+        res.status(200).json(users);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
 });
 
 
@@ -18,10 +20,14 @@ app.get('/users', (req, res) => {
 Usado para pegar alguma coisa que foi criada no nosso banco
 */
 
-app.post('/users', (req, res) => {
-    const user = UserModel.create(req.body)
-    res.status(201).json(user); //201 diz que o registro que eu quero criar, foi criado com sucesso.
-})
+app.post('/users', async(req, res) => {
+    try {
+        const user = await UserModel.create(req.body)
+        res.status(201).json(user); //201 diz que o registro que eu quero criar, foi criado com sucesso.
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
 
 const port = 8080;
 /*aqui eu quebrei a cabeça porque botei "app.linsten = (port,..."*/
