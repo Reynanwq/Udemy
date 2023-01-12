2e0c7e8e750705b7d9f1c48054b426c394a5cd2f
Reynan Paiva <reynanwq@gmail.com>
2023-01-12 04:56:59 -0300

    player model completo

---
+++ No-Udemy/NodeJS/Training/src/model/player.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const playerSchema = new mongoose.Schema({
+    Team: {
+        type: String,
+        required: true,
+    },
+    Name: {
+        type: String,
+        required: true,
+    },
+    Role: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+    },
+});
+
+const playerModel = mongoose.model("player", playerSchema);
+module.exports = playerModel;
