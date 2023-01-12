9d0ffc4167078ec6796d8836f046a7b30c106a78
Reynan Paiva <reynanwq@gmail.com>
2023-01-12 05:01:01 -0300

    adicionando conexão

--- No-Udemy/NodeJS/Training/index.js
+++ No-Udemy/NodeJS/Training/index.js
@@ -1,3 +1,11 @@
+/*Para o uso do dotenv, usamos os seguintes comandos*/
+
+const dotenv = require('dotenv');
+const connectToDatabase = require('./src/database/connect');
+dotenv.config();
+connectToDatabase();
+
+/*Todos os require ficam após esses comandos */
 require('./modules/t1');
 require('./modules/dk');
 require('./modules/drx');
@@ -8,8 +16,3 @@ require('./modules/lsb');
 require('./modules/ns');
 require('./modules/brion');
 require('./modules/hle');
-
-/*Para o uso do dotenv, usamos os seguintes comandos*/
-
-const dotenv = require('dotenv');
-dotenv.config();
