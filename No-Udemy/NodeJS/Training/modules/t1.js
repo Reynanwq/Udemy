df1ddf3cb765318ecf737b86b806f45254b5e08d
Reynan Paiva <reynanwq@gmail.com>
2023-01-12 05:07:23 -0300

    preparando escopo do t1.js

--- No-Udemy/NodeJS/Training/modules/t1.js
+++ No-Udemy/NodeJS/Training/modules/t1.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 5050;
+
+app.use(express.json()); //Facilitar o uso do json em nossas requisições
+
+/* -------------- EXIBIR NO TERMINAL ------------ */
+
+/* -------------  Pegar o player pelo ID ------------ */
+
+/* -------------Criar Player  -----------
+POST: Usado para pegar alguma coisa que foi criada no nosso banco
+*/
+
+/*-------------------- ATUALIZAR UM PLAYER ---------------- */
+
+/*-------------------- DELETAR UM PLAYER ---------------- */
+
+
 //1° parametro é o diretório || 2° parametro é o que eu vou enviar
 app.get('/t1', function(req, res) {
     res.status(200).send(
