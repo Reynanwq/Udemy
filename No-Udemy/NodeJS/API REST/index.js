eb5b09673c0b017eed286ee1c69247dce577c2a9
Reynan Paiva <reynanwq@gmail.com>
2023-01-15 12:09:53 -0300

    servidor rodando + parando para o almoço

--- No-Udemy/NodeJS/API REST/index.js
+++ No-Udemy/NodeJS/API REST/index.js
@@ -4,11 +4,22 @@
 
 const express = require('express');
 const app = express();
+const port = 3000;
+/*Forma de ler JSON: para isso utilizamos midwalers*/
 
-/*Forma de ler JSON*/
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
 
+app.use(express.json())
 
 /*Rota inical / endpoint */
-
+app.get('/home', (req, res) => {
+    res.json({ message: 'oi express' })
+});
 
 /*Entregar uma porta*/
+
+app.listen(port);
