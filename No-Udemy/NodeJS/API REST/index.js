41b314d623946c8888b905f1f1d31c227c6c68d0
Reynan Paiva <reynanwq@gmail.com>
2023-01-15 14:23:19 -0300

    conexao com o mongoDB realizada + iniciando criação de rotas da API

--- No-Udemy/NodeJS/API REST/index.js
+++ No-Udemy/NodeJS/API REST/index.js
@@ -5,7 +5,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose'); //importando o mongoose 
+const DB_USER = 'reynan';
+const DB_PASSWORD = encodeURIComponent('reynann1234');
+const Person = require('./models/Person')
+
+app.use(express.json());
 /*Forma de ler JSON: para isso utilizamos midwalers*/
+
+
+/* -------------- ROTAS DA API  ---------------*/
+
 
 app.use(
     express.urlencoded({
@@ -13,19 +23,19 @@ app.use(
     }),
 )
 
-app.use(express.json())
 
 /*Rota inical / endpoint */
 app.get('/home', (req, res) => {
     res.json({ message: 'oi express' })
 });
 
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@apirest.5yq1hjx.mongodb.net/?retryWrites=true&w=majority`)
+    .then(() => {
+        console.log('Conectamos ao mongoDB')
+        app.listen(port);
+    })
+    .catch((err) => console.log(err));
 /*Entregar uma porta*/
 
 app.listen(port); //deu um pequeno erro com o listen, mas já foi resolvido
-
-//login: reynan
-//password: reynann1234
-
-
-// mongodb+srv://reynan:<password>@apirest.5yq1hjx.mongodb.net/?retryWrites=true&w=majority
