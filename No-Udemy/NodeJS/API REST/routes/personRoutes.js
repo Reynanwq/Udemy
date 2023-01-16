5a2e1fca4ac39b874ea5e7d09ff9830e0fe3b04d
Reynan Paiva <reynanwq@gmail.com>
2023-01-15 23:02:03 -0300

    deletando usuarios

--- No-Udemy/NodeJS/API REST/routes/personRoutes.js
+++ No-Udemy/NodeJS/API REST/routes/personRoutes.js
@@ -80,6 +80,24 @@ router.patch('/:id', async(req, res) => {
     }
 });
 
+
+/* --------------------- DELETANDO DADOS DO USUARIO ----------------- */
+
+router.delete('/:id', async(req, res) => {
+    const id = req.params.id;
+    const person = await Person.findOne({ _id: id })
+    if (!person) {
+        res.status(422).json({ message: 'O usuario que você deseja deletar não foi encontrado' });
+        return;
+    }
+    try {
+        await Person.deleteOne({ _id: id }); //filtando
+        res.status(200).json({ message: 'usuario removido com sucesso' })
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+});
+
 /* -------------- FINAL ROTAS DA API ---------------*/
 
 module.exports = router;
