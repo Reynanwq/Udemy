47eea7d6be1d3ef1b76896650964a1c4bda3ac82
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2023-01-19 22:38:23 -0300

    what's vue.js

---
+++ No-Udemy/What's/Vue.js
@@ -0,0 +1,17 @@
+É um framework
+
+criado por um ex-funcionário do google, foi criado com o objetivo de ser um framework melhor que o angular.
+
+Vue x angular x react
+
+
+é progressivo e reativo.
+
+reativo: html será atualizado constantemente e instantaneamente, automaticamente
+progressivo: fácil de ser utilizado em qualquer momento da aplicação
+
+muito similar com o react
+
+não é preciso trabalhar com várias bibliotecas em conjunto.
+
+vue devtols: instalar para facilitar o trabalho com o vue.js
